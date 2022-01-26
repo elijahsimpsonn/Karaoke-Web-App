8512253b6dbf0d2775023699a7fe7c18395c0c1e
Elijah Simpson <elijahsimpson1991@gmail.com>
2022-01-25 20:14:37 -0500

    adding mock data

--- src/Containers/Dashboard.js
+++ src/Containers/Dashboard.js
@@ -2,25 +2,53 @@ import React from "react";
 import Button from "../Components/Button/Button";
 import Search from "../Components/Search/Search";
 import LongSelection from "../Components/LongSelection/LongSelection";
-import './Dashboard.css'
+import "./Dashboard.css";
 
 export default function Dashboard() {
   // Mock data just to get something going on the frontend while I figure out the backend
   const title = "Marshall Entertainment";
-  const searchPlaceholder = "Search By Artist..."
+  const searchPlaceholder = "Search By Artist...";
+  const mockData = [
+    {
+      artist: "AC/DC",
+      title: "Back In Black",
+    },
+    {
+      artist: "ACTION BRONSON",
+      title: "Action Crazy",
+    },
+    {
+      artist: 'Adele',
+      title: '',
+    },
+    {
+      artist: 'Aerosmith',
+      title: '',
+    },
+    {
+      artist: 'Al Green',
+      title: '',
+    },
+    {
+      artist: 'alabama',
+      title: '',
+    },
+  ];
 
   return (
     <div className="dashboard">
       <div className="title">{title}</div>
       <Button title="VIEW WEEKLY SCHEDULE" />
-      <hr className="line"/>
+      <hr className="line" />
       <div className="selection-buttons">
-          <Button title="VIEW BY ARTIST"/>
-          <Button title="VIEW BY TITLE" btnClass="title-btn"/>
+        <Button title="VIEW BY ARTIST" />
+        <Button title="VIEW BY TITLE" btnClass="title-btn" />
       </div>
-      <Search placeholder={searchPlaceholder}/>
+      <Search placeholder={searchPlaceholder} />
       <div>
-          <LongSelection artist="test"/>
+        {mockData.map((obj) => {
+          return <LongSelection artist={obj.artist} />;
+        })}
       </div>
     </div>
   );
