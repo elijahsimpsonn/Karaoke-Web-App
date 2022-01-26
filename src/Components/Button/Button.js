334e1795c0b462dfbecc5f1ea5a65bb9e8de8940
Elijah Simpson <elijahsimpson1991@gmail.com>
2022-01-25 20:30:35 -0500

    some btn logic

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -2,5 +2,6 @@ import React from "react";
 import "./Button.css";
 
 export default function Button(props) {
-  return <div className={props.btnClass ? `button ${props.btnClass}` : 'button'}>{props.title}</div>;
+  const {btnClass, onClick, title} = props
+  return <div className={btnClass ? `button ${btnClass}` : 'button'} onClick={() => onClick && onClick()}>{title}</div>;
 }
