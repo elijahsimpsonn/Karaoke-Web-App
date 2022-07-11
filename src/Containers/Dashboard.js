284ed7452f6b58e9550574d9744a75f34030d207
Elijah Simpson <elijahsimpson1991@gmail.com>
2022-07-10 20:06:56 -0400

    it is dirty but duplicate artist are not longer showing in the artist list

--- src/Containers/Dashboard.js
+++ src/Containers/Dashboard.js
@@ -7,7 +7,7 @@ import "./Dashboard.css";
 export default function Dashboard() {
   // Mock data just to get something going on the frontend while I figure out the backend
 
-  const [selectedBtn, setSelectedBtn] = useState("artist");
+  const [selectedBtn, setSelectedBtn] = useState("title");
   const [searchValue, setSearchValue] = useState("");
   const [selectedArtist, setSelectedArtist] = useState("");
 
@@ -23,8 +23,28 @@ export default function Dashboard() {
       title: "Back In Black",
     },
     {
+      artist: "AC/DC",
+      title: "t.n.t",
+    },
+    {
+      artist: "AC/DC",
+      title: "big balls",
+    },
+    {
+      artist: "AC/DC",
+      title: "Dirty Deeds Done Dirt Cheap",
+    },
+    {
       artist: "ACTION BRONSON",
-      title: "Action Crazy",
+      title: "Actin Crazy",
+    },
+    {
+      artist: "ACTION BRONSON",
+      title: "baby blue",
+    },
+    {
+      artist: "ACTION BRONSON",
+      title: "Imported Goods",
     },
     {
       artist: "Adele",
@@ -45,11 +65,12 @@ export default function Dashboard() {
   ];
 
   const [filteredSongs, setFilteredSongs] = useState(mockData);
+  const [fliteredArtist, setFilteredArtist] = useState(mockData);
 
   const setNewSelectedArtist = (artist) => {
-    setSelectedArtist(artist)
-    console.log(selectedArtist)
-  }
+    setSelectedArtist(artist);
+    console.log(selectedArtist);
+  };
 
   const filterSongs = (event) => {
     const value = event.target.value;
@@ -66,36 +87,49 @@ export default function Dashboard() {
     setSearchValue(value);
   };
 
+  const filterArtist = () => {
+    const results = mockData
+      .map((object) => object.artist)
+      .filter((value, index, self) => self.indexOf(value) === index);
+    setFilteredArtist(results);
+  };
+
+  console.log(fliteredArtist);
+
   return (
     <div className="dashboard">
-      <div className="title">{title}</div>
+      <div className="title"> {title} </div>{" "}
       <Button
         title="VIEW WEEKLY SCHEDULE"
         onClick={() => setSelectedBtn("schedule")}
         active={selectedBtn === "schedule"}
-      />
+      />{" "}
       <hr className="line" />
       <div className="selection-buttons">
         <Button
           title="VIEW BY ARTIST"
-          onClick={() => setSelectedBtn("artist")}
+          onClick={() => {
+            filterArtist();
+            setSelectedBtn("artist");
+          }}
           active={selectedBtn === "artist"}
-        />
+        />{" "}
         <Button
           title="VIEW BY TITLE"
           btnClass="title-btn"
           onClick={() => setSelectedBtn("title")}
           active={selectedBtn === "title"}
-        />
-      </div>
+        />{" "}
+      </div>{" "}
       {selectedBtn !== "schedule" && (
         <Search
           placeholder={searchPlaceholder}
-          onChange={filterSongs}
+          onChange={selectedBtn === "title" ? filterSongs : filterArtist}
           value={searchValue}
         />
-      )}
+      )}{" "}
       <div>
+        {" "}
         {selectedBtn === "title" &&
           filteredSongs
             .sort((a, b) => a.title.localeCompare(b.title))
@@ -106,20 +140,20 @@ export default function Dashboard() {
                 artist={song.artist}
                 title={song.title}
               />
-            ))}
+            ))}{" "}
         {selectedBtn === "artist" &&
-          filteredSongs
-            .sort((a, b) => a.artist.localeCompare(b.artist))
-            .map((song, index) => (
+          fliteredArtist
+            .sort((a, b) => a.localeCompare(b))
+            .map((artist, index) => (
               <LongSelection
                 key={index}
                 type={selectedBtn}
-                artist={song.artist}
-                onClick={() => setNewSelectedArtist(song.artist)}
-                selected={song.artist === selectedArtist}
+                artist={artist}
+                onClick={() => setNewSelectedArtist(artist)}
+                selected={artist === selectedArtist}
               />
-            ))}
-      </div>
+            ))}{" "}
+      </div>{" "}
     </div>
   );
 }
