04fc2f374132d41a6edd72fedac161e180a132cc
Elijah Simpson <elijahsimpson1991@gmail.com>
2022-07-14 18:41:17 -0400

    added route to admin page

--- src/Containers/Router.js
+++ src/Containers/Router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "../Containers/Dashboard/Dashboard";
+import Admin from "./Admin/Admin";
 import './Router.css'
 
 export default function Router() {
@@ -9,6 +10,7 @@ export default function Router() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/admin" element={<Admin />} />
         </Routes>
       </BrowserRouter>
     </>
